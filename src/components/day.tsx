b74f688825e4109094ddf9f8852c0b2f50536718
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-03-28 00:28:49 +0200

    feat: day cell is button now, first/last day with month name, current month are different color from another month

--- src/components/day.tsx
+++ src/components/day.tsx
@@ -4,15 +4,21 @@ import { css } from "@emotion/react";
 
 import { useLocale } from "@/hooks/useLocale";
 import { isFirstDayOfMonth, isLastDayOfMonth } from "@/lib/utils";
+import { SelectedDateCtx } from "@/context/selectedDateCtx";
 
 const styles = {
 	dayCell: css({
 		display: "flex",
 		flexDirection: "column",
+		border: "unset",
 		borderInlineEnd: "1px solid lightgrey",
 		fontSize: "0.875rem",
 		lineHeight: "1.25rem",
 		padding: "0.25rem",
+	}),
+
+	dayCellDimmed: css({
+		backgroundColor: "transparent",
 	}),
 
 	dayCellHeader: css({
@@ -50,13 +56,23 @@ export const Day = ({ day }: { day: Date }) => {
 
 	const isToday = new Date().toDateString() === day.toDateString();
 
+	const { date: newDate } = React.useContext(SelectedDateCtx);
+	const selectedMonth = new Date(newDate).getMonth();
+	const isCurrentMonth = day.getMonth() === selectedMonth;
+
 	return (
-		<div css={styles.dayCell}>
-			<header css={styles.dayCellHeader}>
+		<button
+			type="button"
+			css={[styles.dayCell, !isCurrentMonth && styles.dayCellDimmed]}
+		>
+			<span hidden aria-hidden="false">
+				Add calendar event
+			</span>
+			<div css={styles.dayCellHeader}>
 				<time css={isToday && styles.todayTime} dateTime={day.toDateString()}>
 					{dayFormat.format(day)}
 				</time>
-			</header>
-		</div>
+			</div>
+		</button>
 	);
 };
