6626353c3af6e172d690abeca9560b77db5f53d3
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-03-29 13:01:48 +0200

    feat: add selected day component for add event form

---
+++ src/components/shared/selectedDay.tsx
@@ -0,0 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import { useStore } from "@nanostores/react";
+
+import { useLocale } from "@/hooks/useLocale";
+import { $selectedDay } from "@/stores/selectedDayStore";
+
+export const SelectedDay = ({
+	formatter,
+}: Record<string, Intl.DateTimeFormatOptions>) => {
+	const locale = useLocale();
+  const selectedDay =useStore($selectedDay);
+	const currentDate = new Date(selectedDay);
+	const intlCurrentDate = new Intl.DateTimeFormat(locale, formatter).format(
+		currentDate,
+	);
+
+	return <time dateTime={new Date().toDateString()}>{intlCurrentDate}</time>;
+};
