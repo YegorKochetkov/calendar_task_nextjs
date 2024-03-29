1c491c2e74a747b63267961ea04b27eed8959ee8
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-03-29 15:36:13 +0200

    feat: save selected data to localstorage

--- src/stores/selectedDateStore.ts
+++ src/stores/selectedDateStore.ts
@@ -1,6 +1,17 @@
-import { atom } from "nanostores";
+import { persistentAtom } from "@nanostores/persistent";
 
-export const $selectedDate = atom<string>(new Date().toDateString());
+export const $selectedDate = persistentAtom<string>("selectedDate", "", {
+	encode(value) {
+		return JSON.stringify(value);
+	},
+	decode(value) {
+		try {
+			return JSON.parse(value);
+		} catch {
+			return new Date().toDateString();
+		}
+	},
+});
 
 export function updateSelectedDate(date: string) {
 	$selectedDate.set(date);
