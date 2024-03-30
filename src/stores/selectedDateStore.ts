d4cc41080eb5d220d5b60c13e7cc2d0b36a309f5
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-03-30 13:07:34 +0200

    feat: set initial data for selectedDate

--- src/stores/selectedDateStore.ts
+++ src/stores/selectedDateStore.ts
@@ -1,17 +1,21 @@
 import { persistentAtom } from "@nanostores/persistent";
 
-export const $selectedDate = persistentAtom<string>("selectedDate", "", {
-	encode(value) {
-		return JSON.stringify(value);
+export const $selectedDate = persistentAtom<string>(
+	"selectedDate",
+	new Date().toDateString(),
+	{
+		encode(value) {
+			return JSON.stringify(value);
+		},
+		decode(value) {
+			try {
+				return JSON.parse(value);
+			} catch {
+				return new Date().toDateString();
+			}
+		},
 	},
-	decode(value) {
-		try {
-			return JSON.parse(value);
-		} catch {
-			return new Date().toDateString();
-		}
-	},
-});
+);
 
 export function updateSelectedDate(date: string) {
 	$selectedDate.set(date);
